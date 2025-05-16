9cb53a9d97330ced97f5f5612b0d3f0237c27585
SUNXU <oca2402ai0001@edu.oca.ac.jp>
2025-05-16 06:31:26 +0000

    practical program 1

--- rpg_battle.rb
+++ rpg_battle.rb
@@ -2,11 +2,54 @@ class Game
     def initialize
         puts "↓勇者の名前を入力してください↓"
         hero_name = gets.chomp
+        
+        @heroes = create_heros("BEN")
+        @monsters = create_monsters
 
-        puts "勇者の名前は#{hero_name}です"
+        display_character_info(@heroes)
+        display_character_info(@monsters)
+
+    end 
+
+    private
+
+    def create_heros(hero_name)
+        Character.new(hero_name,30,6,Constants::ATTACK_TYPE_NORMAL,true)
+    end
+
+    def create_monsters
+        Character.new("オーク",30,8,Constants::ATTACK_TYPE_NORMAL)
+    end
+
+    def display_character_info(character)
+        puts "\nキャラクター名：#{character.name}"
+        puts "HP：#{character.hp}"
+        puts "こうげき力：#{character.attack_damage}"
+        puts "こうげきタイプ：#{character.attack_type}"
+        puts "プレイヤーフラグ：#{character.is_player}"
+        puts "生存フラグ：#{character.is_alive}"
+    end
+    
+end
+
+
+
+class Character
+    attr_accessor :name,:hp,:attack_damage,:attack_type,:is_player,:is_alive
+
+    def initialize(name,hp,attack_damage,attack_type,is_player=false)
+        @name = name
+        @hp = hp
+        @attack_damage = attack_damage
+        @attack_type = attack_type
+        @is_player = is_player
+        @is_alive = true
     end 
 end
 
+class Constants
+    ATTACK_TYPE_NORMAL = 1
+    ATTACK_TYPE_MAGIC = 2
+end
+
 Game.new
-
-
